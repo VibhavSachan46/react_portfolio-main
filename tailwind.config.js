f79980dccf4aba9f06f6c9cd78b2a4fb5a78b89b
Vibhav <vibhavsachan743@gmail.com>
2024-08-27 22:05:55 +0530

    Font adjustment

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,8 +9,7 @@ module.exports = {
         inter: ["Inter", "sans-serif"],
         "edu-sa": ["Edu SA Beginner", "cursive"],
         mono: ["Roboto Mono", "monospace"],
-        Madimi: ["Salsa", "cursive"],
-        Heading: ["Niconne", "cursive"]
+        Heading: ["cursive"]
       },
       colors: {
         // Configure your color palette here
