d4f385ddb239265bc05d90518edf828aedc3149b
Vibhav <vibhavsachan743@gmail.com>
2024-08-29 12:50:05 +0530

    Improved Project Section with project status and add new project

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -6,6 +6,7 @@ import todo from "../assets/image/todo.png";
 import google from "../assets/image/google.png";
 import rock from "../assets/image/rock.png";
 import FileSharing from "../assets/image/FileSharing.png"
+import LinkNest from "../assets/image/LinkNest.png"
 
 import "swiper/css";
 import "swiper/css/pagination";
@@ -17,7 +18,18 @@ const Project = () => {
       name: "Story Scape",
       github_link: "https://github.com/VibhavSachan46/Blog-Completed",
       live_link: "https://story-scape.vercel.app/",
-      tech: "JavaScript, ReactJS, NodeJS, ExpressJS, MongoDB, Tailwind CSS, Cloudinary, Redux Toolkit",
+      tech: "JS, ReactJS, NodeJS, ExpressJS, MongoDB, Tailwind CSS, Cloudinary, Redux Toolkit",
+      videoURL: "https://res.cloudinary.com/ddnpto5af/video/upload/v1724913613/Project%20Videos/StoryScape_s9f6js.mp4",
+      status: "Completed"
+    },
+    {
+      img: LinkNest,
+      name: "Link Nest",
+      github_link: "https://github.com/VibhavSachan46/Blog-Completed",
+      live_link: "",
+      tech: "Type Script, NextJS, NodeJS, MongoDB, Tailwind CSS, Cloudinary, JWT",
+      videoURL: "https://res.cloudinary.com/ddnpto5af/video/upload/v1724915604/Project%20Videos/linbknest_demo_1_yb4fyx.mp4",
+      status: "In Progress"
     },
     {
       img: FileSharing,
@@ -25,6 +37,7 @@ const Project = () => {
       github_link: "https://github.com/VibhavSachan46/filesharing",
       live_link: "https://flick-files.vercel.app/",
       tech: "JavaScript, ReactJS, NodeJS, ExpressJS, MongoDB, Tailwind CSS, Cloudinary",
+      status: "In Progress"
     },
     {
       img: paytm,
@@ -32,6 +45,7 @@ const Project = () => {
       github_link: "https://github.com/VibhavSachan46/Paytm",
       live_link: "https://paytm-frontend-two.vercel.app/signin",
       tech: "JavaScript, ReactJS, NodeJS, ExpressJs, MongoDB, Tailwind CSS, JWT",
+      status: "Completed"
     },
     {
       img: weather,
@@ -39,6 +53,7 @@ const Project = () => {
       github_link: "https://github.com/VibhavSachan46/weather1",
       live_link: "https://weather-ten-drab.vercel.app/",
       tech: "JavaScript, ReactJS",
+      status: "Completed"
     },
     {
       img: google,
@@ -46,6 +61,7 @@ const Project = () => {
       github_link: "https://github.com/VibhavSachan46/google-search-app-clone",
       live_link: "https://gooogle-search-app.netlify.app/",
       tech: "JavaScript, ReactJs, Tailwind CSS",
+      status: "Completed"
     },
     {
       img: todo,
@@ -53,6 +69,7 @@ const Project = () => {
       github_link: "https://github.com/VibhavSachan46/Todo-JS",
       live_link: "https://vibhavsachan46.github.io/Todo-JS/",
       tech: "HTML, CSS, JavaScript",
+      status: "Completed"
     },
     {
       img: rock,
@@ -60,8 +77,9 @@ const Project = () => {
       github_link: "https://github.com/VibhavSachan46/rock-paper-scissor",
       live_link: "https://vibhavsachan46.github.io/rock-paper-scissor/",
       tech: "HTML, CSS, JavaScript",
+      status: "Completed"
     },
-    
+
   ];
   return (
     <section id="projects" className="py-10 text-white">
@@ -74,28 +92,52 @@ const Project = () => {
       <div className="px-32">
         <div className="grid gap-10 sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-3 lg:px-5">
           {projects.map((project_info, i) => (
-            <div key={i} className="flex flex-col bg-slate-700 p-4 rounded-xl justify-center items-center">
+            <div
+              key={i}
+              className="flex flex-col bg-slate-700 p-4 rounded-3xl justify-center items-center"
+            >
               <h3 className="text-3xl font-Heading my-4 ">{project_info.name}</h3>
               <img src={project_info.img} alt="" className="rounded-lg w-full" />
               <p className="p-2 lg:font-Heading">{project_info.tech}</p>
 
+              {/* Status Indicator */}
+              <div className={`status-indicator ${project_info.status === 'In Progress' ? 'bg-yellow-500 hover:bg-yellow-700 hover:cursor-pointer' : 'bg-green-500 hover:bg-green-700 hover:cursor-pointer'} text-white rounded-full px-4 py-2 mt-4 mb-4`}>
+                {project_info.status}
+              </div>
+
               <div className="flex gap-3 mt-4">
-                <a
-                  href={project_info.github_link}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  className="text-cyan-600 bg-gray-800 px-2 py-1 inline-block rounded"
-                >
-                  Github
-                </a>
-                <a
-                  href={project_info.live_link}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  className="text-cyan-600 bg-gray-800 px-2 py-1 inline-block rounded"
-                >
-                  Live Demo
-                </a>
+                {project_info.github_link && (
+                  <a
+                    href={project_info.github_link}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="text-white bg-gray-800 px-2 py-2 inline-block rounded-2xl  hover:bg-gray-900 hover:text-cyan-600 justify-center items-center"
+                  >
+                    Github
+                  </a>
+                )}
+
+                {project_info.live_link && (
+                  <a
+                    href={project_info.live_link}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="text-white bg-gray-800 px-2 py-2 inline-block rounded-2xl  hover:bg-gray-900 hover:text-cyan-600 justify-center items-center"
+                  >
+                    Live Project
+                  </a>
+                )}
+
+                {project_info.videoURL && (
+                  <a
+                    href={project_info.videoURL}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="text-white bg-gray-800 px-2 py-2 inline-block rounded-2xl  hover:bg-gray-900 hover:text-cyan-600 justify-center items-center"
+                  >
+                    Watch Video
+                  </a>
+                )}
               </div>
             </div>
           ))}
